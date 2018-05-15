d9bd6b763744632431d34d4abe0c6faef9a4c11d
MaAnShanGuider <2388540124@qq.com>
2018-05-15 18:21:11 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+
+({
+	run: function(){
+		console.log('项目初始化');
+		this.renderCanvas();
+		this.handleClick();
+	},
+	paramters: {
+		points: {
+			start: null,
+			end: null
+		}
+	},
+	// 绘制地图
+	renderCanvas: function(){
+
+	},
+	// 点击事件
+	handleClick: function(){
+	},
+	boom:function(){
+		console.log('dfaaf');
+	}
+}).run();
