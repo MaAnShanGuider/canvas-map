31c597038d63cba0b137b6e43b9c85427aa22143
MaAnShanGuider <2388540124@qq.com>
2018-05-19 17:59:55 +0800

    添加了注释

--- js/Util.js
+++ js/Util.js
@@ -2,6 +2,7 @@
 
 var MathUtil = {
     randomInt: function randomInt(a, b) {
+    	
         if (typeof b === 'undefined') {
             a = a || 1;
             return Math.floor(Math.random() * a);
--- js/maze.js
+++ js/maze.js
@@ -50,6 +50,23 @@ var Maze = function() {
     }, {
         key: 'initArray',
         value: function initArray() {
+/**
+ * 数组初始化 
+ * 
+ * 他这里其实就是在判断 n 的二进制有没有进位，
+ * 进位了，就说明，n, (n-1)在最大位上都是1，最小位上n为 1,(n-1)为0。
+ * 那么他们的差自然为1。满足条件。
+ *
+ * 也就是说，这里判断的是，n 是否为奇偶数。
+ * n 为奇数，那么 n ^ (n-1) === 1, 满足条件;
+ * n 为偶数，那么 n ^ (n-1) > 1 不满足条件;
+ *
+ * 
+ * 不过这个notAccessed是什么作用
+ * notAccessed的数组的作用是: 
+ *     当前所有的通路格子对应的坐标的索引，为0，即表示通路
+ *                                      为1，即表示断路
+ */
             for (var i = 0; i < 2 * this.r + 1; ++i) {
                 this.arr[i] = [];
                 for (var n = 0; n < 2 * this.c + 1; ++n) {
@@ -63,9 +80,20 @@ var Maze = function() {
             }
             this.arr[1][0] = 0;
             this.arr[2 * this.r - 1][2 * this.c] = 0;
-            console.log(JSON.parse(JSON.stringify(this.arr)), 'aa');
         }
-    }, {
+    },
+/**
+ * 数组生成
+ * count: 当前通路格子的全部数量，arr[1][0] 和 arr[2 * this.r - 1][2 * this.c]不记在内
+ * 随机找到一个当前的通路格子。
+ *
+ * tr,tc其实是通路的坐标
+ * 将所有的通路格子看成一个坐标系，因为刚开始，count = this.r * this.c ,即所有的通路格子，
+ * 即r列，c行组成的坐标系，
+ * 那么 (count / this.c) 的整除部分，就是第几行， 余数部分，就是第几列。
+ * 所以，tr, tc是坐标。
+ */
+     {
         key: 'generate',
         value: function generate() {
             var count = this.r * this.c;
@@ -76,23 +104,40 @@ var Maze = function() {
             this.accessed.push(cur);
             this.notAccessed[cur] = 1;
 
+            console.log('cur:', cur);
+            console.log(JSON.parse(JSON.stringify(this.accessed)));
+            console.log(JSON.parse(JSON.stringify(this.notAccessed)));
+
             while (this.accessed.length < count) {
+                /**
+                 * 每次随机挑选一个通路坐标。
+                 * num的取值范围是: 1, 2, 3, 4。刚好是四个方向
+                 * @type {[type]}
+                 */
                 var tr = Math.floor(cur / this.c),
                     tc = cur % this.c;
                 var num = 0,
                     off = -1;
 
+                console.log(tr, tc, '坐标');
                 while (++num < 5) {
                     var around = MathUtil.randomInt(0, 4),
                         nr = tr + offr[around],
                         nc = tc + offc[around];
+                        /**
+                         * nr >= 0 && nc >= 0 && nr < this.r && nc < this.c
+                         * 表示的是，nr, nc 必须在count的坐标系内。
+                         */
+                    console.log('num:', num);
                     if (nr >= 0 && nc >= 0 && nr < this.r && nc < this.c && this.notAccessed[cur + offs[around]] === 0) {
+                        console.log('走到我这里了', num);
                         off = around;
                         break;
                     }
                 }
 
                 if (off < 0) {
+                    console.log('不可能小于0');
                     cur = this.accessed[MathUtil.randomInt(0, this.accessed.length)];
                 } else {
                     tr = 2 * tr + 1;
@@ -123,8 +168,8 @@ var Maze = function() {
             this.canvas.width = window.innerWidth;
             this.canvas.height = window.innerHeight;
 
-            this.canvas.width = "600";
-            this.canvas.height = "600";
+            this.canvas.width = "1200";
+            this.canvas.height = "1200";
             document.body.appendChild(this.canvas);
         }
     }, {
